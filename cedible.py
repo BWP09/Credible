aa5ce2967afe4cbde9f0ef5a297ec29b9af53221
BWP09 <bwpayne2@gmail.com>
2023-01-26 18:20:41 -0700

    init

---
+++ cedible.py
@@ -0,0 +1,9 @@
+# (satire) #
+
+website = input("\nEnter your website URL: ")
+
+if "wikipedia" in website:
+    print("Your website is not credible... sorry.")
+
+else:
+    print("Your website is credible!")
